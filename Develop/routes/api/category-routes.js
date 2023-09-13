f7d50b1dc30278ac9bdad92f2587f2f25ecb7478
JaKrau <jakeziah@gmail.com>
2023-09-12 19:25:35 -0700

    Add Product Routes

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -51,7 +51,7 @@ router.put('/:id', async (req, res) => {
       }
     });
     if (!categoryData) {
-      res.status(404).json({ message: 'No category located with that id.' });
+      res.status(404).json({ message: 'No category found with that id.' });
       return;
     }
     res.status(200).json(categoryData);
@@ -64,14 +64,14 @@ router.put('/:id', async (req, res) => {
 
 router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
-  try{
+  try {
     const categoryData = await Category.destroy({
       where: {
         id: req.params.id
       }
     });
     if (!categoryData) {
-      res.status(404).json({ message: 'No category located with that id.' });
+      res.status(404).json({ message: 'No category found with that id.' });
       return;
     }
     res.status(200).json(categoryData);
